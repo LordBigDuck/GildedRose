7b8db5ef02d8e7ee7c06819d1b45a5ebb6105877
xpiekara <piekara.xavier@gmail.com>
2023-11-15 20:38:40 +0100

    Add constants

---
+++ csharpcore/GildedRose/ItemNames.cs
@@ -0,0 +1,9 @@
+﻿namespace GildedRoseKata;
+
+public static class ItemNames
+{
+    public const string AgedBrie = "Aged Brie";
+    public const string Conjured = "Conjured";
+    public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+    public const string TAFKAL80ETCBackstagePass = "Backstage passes to a TAFKAL80ETC concert";
+}
---
+++ csharpcore/GildedRose/Quality.cs
@@ -0,0 +1,7 @@
+﻿namespace GildedRoseKata;
+
+public static class Quality
+{
+    public const int MaxQuality = 50;
+    public const int SulfurasQuality = 80;
+}
