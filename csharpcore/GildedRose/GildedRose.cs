fe941764a2eed6e48b54fd26b923140ca1f9ec7c
xpiekara <piekara.xavier@gmail.com>
2023-11-22 14:52:04 +0100

    Refactor GildedRose algorithm

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace GildedRoseKata;
 
@@ -16,91 +15,13 @@ public class GildedRose
     {
         foreach (var item in _items)
         {
-            item.Quality = GetNewQuality(item);
-            
-            if (item.Name != ItemNames.Sulfuras)
-            {
-                item.SellIn -= SellIn.DecrementValue;
-            }
-
-            if (item.SellIn < 0)
-            {
-                if (item.Name != ItemNames.AgedBrie)
-                {
-                    if (item.Name != ItemNames.TAFKAL80ETCBackstagePass)
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != ItemNames.Sulfuras)
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < Quality.Max)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
-            }
+            UpdateQuality(item);
         }
     }
 
-    private static int GetTotalQualityToDecrease(Item item)
+    private static void UpdateQuality(Item item)
     {
-        if (item.Quality > Quality.Min)
-        {
-            if (item.Name != ItemNames.Sulfuras)
-            {
-                return 1;
-            }
-        }
-    
-        return 0;
-    }
-    
-    private static int GetNewQuality(Item item)
-        => item.Name switch
-        {
-            ItemNames.Sulfuras => item.Quality + 0,
-            ItemNames.AgedBrie => item.Quality + GetQualityIncrementOfBasicItem(item),
-            ItemNames.TAFKAL80ETCBackstagePass => item.Quality + GetTotalQualityToIncreaseOfBackstagePass(item),
-    
-            _ => item.Quality - GetTotalQualityToDecrease(item)
-        };
-
-    private static int GetQualityIncrement(int quality, int toAdd)
-        => quality + toAdd > Quality.Max
-            ? Quality.Max - quality
-            : toAdd;
-
-    private static int GetQualityIncrementOfBasicItem(Item item)
-        => GetQualityIncrement(item.Quality, Quality.BaseIncrement);
-    
-    private static int GetTotalQualityToIncreaseOfBackstagePass(Item item)
-    {
-        if (!item.Name.Equals(ItemNames.TAFKAL80ETCBackstagePass, StringComparison.InvariantCultureIgnoreCase))
-        {
-            throw new Exception("Wrong item");
-        }
-    
-        var total = Quality.BaseIncrement;
-        if (item.SellIn <= 10)
-        {
-            total += 1;
-            if (item.SellIn <= 5)
-            {
-                total += 1;
-            }
-        }
-
-        return GetQualityIncrement(item.Quality, total);
+        item.SellIn = SellInCalculator.ComputeSellIn(item);
+        item.Quality = QualityCalculator.ComputeNewQuality(item);
     }
 }
